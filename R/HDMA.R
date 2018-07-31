8e7b6c58041f6f8686af0bac70c5624af63d8231
jumentib <jumentib@eduroam-107059.grenet.fr>
2018-07-31 13:28:16 +0200

    ortho3

--- R/HDMA.R
+++ R/HDMA.R
@@ -1,4 +1,4 @@
-#' r_mediation : function to simulate DNA methylation data for mediation analyzis
+#' r_mediation : function to simulate DNA methylation data for mediation analyzi
 #'
 #' @param n	: number of individuals
 #' @param p	: number of cpg variables
